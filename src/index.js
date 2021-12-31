e65b7acbf4f645dd5af779c4ea06e5acff410106
kevinarauz <kecin222294@gmail.com>
2021-12-30 19:33:02 -0500

    Se agrega configuracion de variables de entorno en puertos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 
 var mongoose = require('mongoose');
 var app = require('../src/app');
-var port = 3700;
+var port = process.env.PORT || 3700;
+
+app.get('/', function (req, res) {
+	res.send({message: 'Microservicio Cliente V.1'});
+	
+});
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://mongo:27017/clientesApi',{
@@ -10,12 +15,11 @@ mongoose.connect('mongodb://mongo:27017/clientesApi',{
 	useUnifiedTopology: true
 })
         .then((db) => {
+			// Creacion del servidor
         	console.log("Conexión a la base de datos establecida satisfactoriamente...");
 			console.log("DB conectada: "+db.connection.host);
-        	// Creacion del servidor
         	app.listen(port, () => {
-        		console.log("Servidor corriendo correctamente en la url: localhost:"+port);
+        		console.log(`Servidor corriendo correctamente en la url: localhost: ${port}`);
         	});
-
         })
         .catch(err => console.log(err));
